353e232ecdb91e5be1a94c4c900f6ed261fff640
dbgheric <165889979+dbgheric@users.noreply.github.com>
2024-04-04 22:08:04 +0900

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,2 +1,16 @@
 def main():
-    return
+    print("Let's implement division. Type two numbers for x and y")
+    
+    x = int(input("x > "))
+    y = int(input("y > "))
+    
+    print("%d / %d = %0.3f" % (x, y, divide(x, y)))
+    
+    
+def add(a, b):
+    result=a+b
+    
+    return result
+    
+
+
